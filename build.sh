2cb0917a27ea2e7eaac50534395673afaf8e4c42
Sam Stephenson <sam@37signals.com>
2011-03-09 11:21:56 -0600

    Add build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+VERSION=$(node -e 'JSON.parse(require("fs").readFileSync("package.json","utf8")).version')
+ROOT=dist/$VERSION
+
+npm bundle
+rm -fr $ROOT
+mkdir -p $ROOT/bin
+cp -R lib node_modules $ROOT
+cp `which node` $ROOT/bin
+cp bin/pow* $ROOT/bin
+cp package.json $ROOT

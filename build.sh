65d9ffcce53bc8aab371007624857e35b4c43e1b
Sam Stephenson <sam@37signals.com>
2011-03-23 22:16:49 -0500

    New build.sh with --install option

--- build.sh
+++ build.sh
@@ -1,15 +1,35 @@
 #!/bin/sh -e
+# `./build.sh` generates dist/$VERSION.tar.gz
+# `./build.sh --install` installs into ~/Library/Application Support/Pow/Current
 
 VERSION=$(node -e 'JSON.parse(require("fs").readFileSync("package.json","utf8")).version')
-ROOT=dist/$VERSION
+ROOT="/tmp/pow-build.$$"
+DIST="$(pwd)/dist"
 
-npm bundle
-rm -fr $ROOT
-mkdir -p $ROOT/bin
-cp -R lib node_modules $ROOT
-cp `which node` $ROOT/bin
-cp bin/pow* $ROOT/bin
-cp package.json $ROOT
+cake build
 
-cd dist
-tar czvf $VERSION.tar.gz $VERSION
+mkdir -p "$ROOT/$VERSION"
+cp -R package.json bin lib "$ROOT/$VERSION"
+cd "$ROOT/$VERSION"
+npm bundle 2> /dev/null
+cp `which node` bin
+
+if [ "$1" == "--install" ]; then
+  POW_ROOT="$HOME/Library/Application Support/Pow"
+  rm -fr "$POW_ROOT/Versions/9999.0.0"
+  mkdir -p "$POW_ROOT/Versions"
+  cp -R "$ROOT/$VERSION" "$POW_ROOT/Versions/9999.0.0"
+  rm -f "$POW_ROOT/Current"
+  cd "$POW_ROOT"
+  ln -s Versions/9999.0.0 Current
+  echo "$POW_ROOT/Versions/9999.0.0"
+else
+  cd "$ROOT"
+  tar czf "$VERSION.tar.gz" "$VERSION"
+  mkdir -p "$DIST"
+  cd "$DIST"
+  mv "$ROOT/$VERSION.tar.gz" "$DIST"
+  echo "$DIST/$VERSION.tar.gz"
+fi
+
+rm -fr "$ROOT"

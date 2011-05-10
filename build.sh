6f193ee41f0853d513e2abfa1ba300508e302524
Sam Stephenson <sam@37signals.com>
2011-05-10 12:17:15 -0500

    Update manual

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-git show master:README.md |
+git show master:MANUAL.md |
+./mdtoc.rb |
 markdown |
 ruby -e 'puts IO.read("manual.html").sub(/(<!--begin-->).*?(<!--end-->)/m) { $1 + $<.read + $2 }' \
 > manual.tmp.html

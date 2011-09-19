6279d402cf86750ea2301f3fa6cf8f9583fa1329
Sam Stephenson <sam@37signals.com>
2011-09-19 08:51:22 -0500

    Remove reference to ndns from build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,6 @@ cake build
 mkdir -p "$ROOT/$VERSION/node_modules"
 cp -R package.json bin lib "$ROOT/$VERSION"
 cp Cakefile "$ROOT/$VERSION"
-cp -R node_modules/ndns "$ROOT/$VERSION/node_modules"
 cd "$ROOT/$VERSION"
 BUNDLE_ONLY=1 npm install >/dev/null
 cp `which node` bin

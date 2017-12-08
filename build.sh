a1515a20e8e7b1fc849ad1aa31f06bafedf1722f
Sam Stephenson <sam@37signals.com>
2017-12-08 16:04:16 -0600

    Set BRANCH= to specify the branch to pull docs from

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 set -e
+BRANCH="${BRANCH:-master}"
 
-git show master:MANUAL.md |
+git show "${BRANCH}:MANUAL.md" |
 ./mdtoc.rb |
 markdown |
 ruby -e 'puts IO.read("manual.html").sub(/(<!--begin-->).*?(<!--end-->)/m) { $1 + $<.read + $2 }' \
